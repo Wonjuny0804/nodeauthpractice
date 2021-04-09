21cf61291077abef96765dccc46261e75b4784cc
WonjunJang <wonjun84@naver.com>
2021-04-09 09:14:43 +0900

    feat: Setup DB

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+
+// Connect to DB
+mongoose.connect(
+  'mongodb+srv://admin:uKUXpq6pJr0Fukrj@cluster0.zpx86.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { 
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+  },
+  () => console.log('connected to mongoDB'));
 
 // Import Routes
 const authRoute = require('./routes/auth');
